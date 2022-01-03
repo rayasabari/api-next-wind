f8134e4673254e174716e8564537e7c7435dfda6
raya <raya@rhp-valuation.com>
2022-01-04 00:55:43 +0700

    init

---
+++ app/Models/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens, HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function gravatar($size = 150)
+    {
+       return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $this->email ) ) ) . "?d=mm&s=" . $size;
+    }
+
+    public function getJoinedAttribute()
+    {
+        return date('y') == $this->created_at->format('y') ? $this->created_at->format('d F') : $this->created_at->format('d F, Y');
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        collect([
+            [
+                'name' => 'Raya',
+                'email' => 'hi@rayasabari.com',
+                'email_verified_at' => null,
+                'password' => bcrypt('password'),
+                'created_at' => now(),
+                'updated_at'  => now(),
+            ],
+            [
+                'name' => 'Eva',
+                'email' => 'eva@rayasabari.com',
+                'email_verified_at' => null,
+                'password' => bcrypt('password'),
+                'created_at' => now(),
+                'updated_at'  => now(),
+            ],
+        ])->each(function ($user){
+            DB::table('users')->insert($user);
+        });
+    }
+}
